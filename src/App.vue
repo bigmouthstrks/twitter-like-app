ba1e1b819c1ced3cf78a0f48cb2e9b84809260a5
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2020-11-23 15:40:00 -0300

    Se agrego vue router al proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,30 @@
 <template>
-    <div id='app'>
-        <nav>
-            <div class="navigation_logo">
-                Twotter
-            </div>
-            <div class="navigation_user">
-                {{ state.user.username }}
-            </div>
-        </nav>
-        <UserProfile />
-    </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import { reactive } from 'vue';
-import UserProfile from './components/UserProfile.vue';
-
-export default {
-    name: 'App',
-    components: { UserProfile },
-    setup(){
-        const state = reactive({
-            user: {
-                username: 'bigmouthstrks'
-            }
-        })
-
-        return {
-            state
-        }
-    }
-}
-</script>
 
 <style lang="scss">
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2C3E50;
-    min-height: 100vh;
-    background-color: #F3F5FA;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-    nav{
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 10px 5%;
-        background-color: lightseagreen;
-        color: white;
+#nav {
+  padding: 30px;
 
-        .navigation_logo{
-            font-weight: bold;
-            font-size: 24px;
-        }
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
+  }
 }
 </style>
